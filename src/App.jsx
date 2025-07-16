2b5a268c6d944335cc9d307448d10962a1001d33
priyankabk123 <priyankabkhandagale@gmail.com>
2025-07-16 14:26:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Hello from Vercel + Vite + React 🚀</h1>
+      <p>This is a sample app deployed on Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
